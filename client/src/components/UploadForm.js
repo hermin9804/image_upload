388c79609d53f156da324cfed0fc78294a65bbd6
hermin9804 <hermin9804@gmail.com>
2023-05-03 01:16:01 +0900

    s2: axios 이용해서 백엔드로 이미지 전송하기

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,22 +1,39 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("이미지 파일을 업로드 해주세요.");
 
+  const imageSelectHandler = (e) => {
+    const imageFile = e.target.files[0];
+    setFile(imageFile);
+    setFileName(imageFile.name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log({ res });
+      alert("업로드 성공");
+    } catch (err) {
+      console.log({ err });
+      alert("업로드 실패");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <lable htmlFor="image">{fileName}</lable>
-      <input
-        id="image"
-        type="file"
-        onChange={(event) => {
-          const imageFile = event.target.files[0];
-          setFile(imageFile);
-          setFileName(imageFile.name);
-        }}
-      />
+      <input id="image" type="file" onChange={imageSelectHandler} />
       <button type="submit">제출</button>
     </form>
   );
