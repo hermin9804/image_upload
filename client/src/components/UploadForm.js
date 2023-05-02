0b73e2b418f5964d4168c18c7c950e2412aa93c7
hermin9804 <hermin9804@gmail.com>
2023-05-03 02:40:13 +0900

    s2: 이미지 업로드 전 미리보기 기능 만들기

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -8,6 +8,7 @@ import ProgressBar from "./ProgressBar";
 const UploadForm = () => {
   const defaultFileName = "이미지 파일을 업로드 해주세요.";
   const [file, setFile] = useState(null);
+  const [imgSrc, setImgSrc] = useState(null);
   const [fileName, setFileName] = useState(defaultFileName);
   const [percent, setPercent] = useState(0);
 
@@ -15,6 +16,11 @@ const UploadForm = () => {
     const imageFile = e.target.files[0];
     setFile(imageFile);
     setFileName(imageFile.name);
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(imageFile);
+    fileReader.onload = (e) => {
+      setImgSrc(e.target.result);
+    };
   };
 
   const onSubmit = async (e) => {
@@ -34,20 +40,31 @@ const UploadForm = () => {
       setTimeout(() => {
         setPercent(0);
         setFileName(defaultFileName);
+        setImgSrc(null);
       }, 3000);
     } catch (err) {
       setPercent(0);
       setFileName(defaultFileName);
+      setImgSrc(null);
       toast.error(err.message);
     }
   };
 
   return (
     <form onSubmit={onSubmit}>
+      <img
+        src={imgSrc}
+        className={`image-preview ${imgSrc && "image-preview-show"}`}
+      />
       <ProgressBar percent={percent} />
       <div className="file-dropper">
         {fileName}
-        <input id="image" type="file" onChange={imageSelectHandler} />
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          onChange={imageSelectHandler}
+        />
       </div>
       <button className="submit-button" type="submit">
         제출
