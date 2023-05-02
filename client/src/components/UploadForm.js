f988fa1329ccbe4d575ee3f20817fa7bfaa070e6
hermin9804 <hermin9804@gmail.com>
2023-05-03 02:28:22 +0900

    s2: 업로드 현황 보여주는 Progress Bar 만들기

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -3,10 +3,13 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "./UploadForm.css";
+import ProgressBar from "./ProgressBar";
 
 const UploadForm = () => {
+  const defaultFileName = "이미지 파일을 업로드 해주세요.";
   const [file, setFile] = useState(null);
-  const [fileName, setFileName] = useState("이미지 파일을 업로드 해주세요.");
+  const [fileName, setFileName] = useState(defaultFileName);
+  const [percent, setPercent] = useState(0);
 
   const imageSelectHandler = (e) => {
     const imageFile = e.target.files[0];
@@ -23,15 +26,25 @@ const UploadForm = () => {
         headers: {
           "Content-Type": "multipart/form-data",
         },
+        onUploadProgress: (e) => {
+          setPercent(Math.round((100 * e.loaded) / e.total));
+        },
       });
       toast.success("이미지 업로드 성공");
+      setTimeout(() => {
+        setPercent(0);
+        setFileName(defaultFileName);
+      }, 3000);
     } catch (err) {
+      setPercent(0);
+      setFileName(defaultFileName);
       toast.error(err.message);
     }
   };
 
   return (
     <form onSubmit={onSubmit}>
+      <ProgressBar percent={percent} />
       <div className="file-dropper">
         {fileName}
         <input id="image" type="file" onChange={imageSelectHandler} />
