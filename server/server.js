ed0c9e23218f216e3e850cf8b9be690965207ec1
hermin9804 <hermin9804@gmail.com>
2023-05-05 18:39:32 +0900

    s3: 사진정보 데이터베이스에서 불러오기

--- server/server.js
+++ server/server.js
@@ -4,12 +4,18 @@ const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
 const mongoose = require("mongoose");
+const Image = require("./models/image");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads"),
-  filename: (req, file, cb) =>
-    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+  filename: (req, file, cb) => {
+    file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );
+    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`);
+  },
 });
+
 const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
@@ -37,9 +43,18 @@ mongoose
 
     app.use("/uploads", express.static("uploads"));
 
-    app.post("/upload", upload.single("image"), (req, res) =>
-      res.json(req.file)
-    );
+    app.post("/images", upload.single("image"), async (req, res) => {
+      const image = await new Image({
+        key: req.file.filename,
+        originalName: req.file.originalname,
+      }).save();
+      res.json(image);
+    });
+
+    app.get("/images", async (req, res) => {
+      const images = await Image.find();
+      res.json(images);
+    });
 
     app.listen(PORT, () => {
       console.log("Server is running on Port: " + PORT);
