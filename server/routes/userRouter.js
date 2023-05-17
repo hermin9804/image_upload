d477e8d1aba2b0669a16c57e91f60f0ab2c37ca5
hermin9804 <hermin9804@gmail.com>
2023-05-17 23:29:08 +0900

    s5: 회원가입 api 마무리

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,24 @@
 const Router = require("express").Router;
 const userRouter = Router();
 const User = require("../models/user");
+const { hash } = require("bcryptjs");
 
 userRouter.post("/register", async (req, res) => {
-  await new User(req.body).save();
-  res.json({ message: "user registered" });
+  try {
+    if (req.body.password < 6)
+      throw new Error("Password must be at least 6 characters");
+    if (req.body.username.length < 3)
+      throw new Error("Username must be at least 3 characters");
+    const hashedPassword = await hash(req.body.password, 10);
+    await new User({
+      name: req.body.name,
+      username: req.body.username,
+      hashedPassword,
+    }).save();
+    res.json({ message: "user registered" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = { userRouter };
