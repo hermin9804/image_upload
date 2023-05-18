65095b3d09fda55515449ab42d07aece48783cab
hermin9804 <hermin9804@gmail.com>
2023-05-18 20:05:45 +0900

    s6: ㄹ로그아웃 처리하기

--- client/src/components/ToolBar.js
+++ client/src/components/ToolBar.js
@@ -1,16 +1,31 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const ToolBar = () => {
   const [me, setMe] = useContext(AuthContext);
+
+  const logoutHandler = async () => {
+    try {
+      await axios.patch("/users/logout");
+      setMe(null);
+      toast.success("로그아웃 성공");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div>
       <Link to="/">
         <span>사진첩</span>
       </Link>
       {me ? (
-        <span style={{ float: "right" }}>로그아웃</span>
+        <span onClick={logoutHandler} style={{ float: "right" }}>
+          로그아웃
+        </span>
       ) : (
         <>
           <Link to="/auth/login">
