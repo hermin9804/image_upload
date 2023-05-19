23ab0c0b261ea2ace8e9a1e0935554edfc80b5ef
hermin9804 <hermin9804@gmail.com>
2023-05-19 22:48:54 +0900

    s7: 권한 확인후 이미지 저장하기

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -4,16 +4,44 @@ const Image = require("../models/image");
 const { upload } = require("../middleware/imageUpload");
 
 imageRouter.post("/", upload.single("image"), async (req, res) => {
-  const image = await new Image({
-    key: req.file.filename,
-    originalName: req.file.originalname,
-  }).save();
-  res.json(image);
+  // todo : 유저 정보, public 유무 확인
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    const image = await new Image({
+      user: {
+        _id: req.user.id,
+        name: req.user.name,
+        username: req.user.username,
+      },
+      public: req.body.public,
+      key: req.file.filename,
+      originalName: req.file.originalname,
+    }).save();
+    res.json(image);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 imageRouter.get("/", async (req, res) => {
+  // public 이미지만 가져오기
   const images = await Image.find();
   res.json(images);
+});
+
+imageRouter.delete("/:imageid", (req, res) => {
+  // 유저 권한 확인
+  // 사진 삭제
+});
+imageRouter.patch("/:imageid/like", (req, res) => {
+  // 유저 권한 확인
+  // like 중복 안되도록 확인
+});
+
+imageRouter.patch("/:imageid/unlike", (req, res) => {
+  // 유저 권한 확인
+  // like 중복 취소 안되도록 확인
 });
 
 module.exports = { imageRouter };
